fcc4959b89be9373bd113fce0e8691d1bf6b7230
Alexander Baggett <alexander.baggett@gmail.com>
2017-02-27 11:13:18 -0600

    Created to answer stack overflow post 42457144

---
+++ stackoverflow42457144/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace stackoverflow42457144
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var video = webBrowser1.Document.GetElementById("video1");
+            video.SetAttribute("muted", "true");
+        }
+    }
+}
